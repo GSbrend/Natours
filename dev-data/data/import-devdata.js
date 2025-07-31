c38ac3d2bd5135533c26a438bd700031e908f8ad
Brendon Garcia <Brendon.gs.dev@gmail.com>
2025-07-31 16:09:44 +0000

    Refactor: Normalize code formatting in import-devdata.js for consistency

--- dev-data/data/import-devdata.js
+++ dev-data/data/import-devdata.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const fs = require('fs');
-const Tour = require('./../../models/tourModule.js');
+const fs = require("fs");
+const Tour = require("./../../models/tourModule.js");
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE.replace(
   "<db_password>",
@@ -15,16 +15,18 @@ mongoose.connect(DB).then(() => {
 });
 
 // reading json
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
+);
 
 // import to db
 const importData = async () => {
-    try{
-        await Tour.create(tours);
-        console.log('Data loaded')
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await Tour.create(tours);
+    console.log("Data loaded");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 console.log(process.argv);
