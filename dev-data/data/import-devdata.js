a147badd8702bf80a9401a666cca65cae9da1511
Brendon Garcia <Brendon.gs.dev@gmail.com>
2025-07-31 16:08:04 +0000

    Refactor: Add import-devdata.js for database connection and data import functionality

---
+++ dev-data/data/import-devdata.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const fs = require('fs');
+const Tour = require('./../../models/tourModule.js');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  "<db_password>",
+  process.env.DB_PASSWORD
+);
+
+mongoose.connect(DB).then(() => {
+  console.log("DB connection successfull");
+});
+
+// reading json
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+// import to db
+const importData = async () => {
+    try{
+        await Tour.create(tours);
+        console.log('Data loaded')
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+console.log(process.argv);
