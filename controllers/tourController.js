61a3a9e57b591074caf2444e6ac15349d8899181
Brendon Garcia <Brendon.gs.dev@gmail.com>
2025-08-03 20:15:55 +0000

    Refactor: Clean up commented code and improve query handling in getAllTours function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,16 +27,29 @@ const Tour = require("../models/tourModel.js");
 
 exports.getAllTours = async (req, res) => {
   try {
-   const tours = await Tour.find(); //finds all data in that collection
-/*
-      const tours = await Tour.find({
-        duration: 5,
-        difficulty: 'easy'
-      }); //finds queied data in the collection
+  //  const tours = await Tour.find(); //finds all data in that collection
+
+
+// BUILDING THE QUERY
+      const queryObj = {...req.query} //take the field out of the object and create a new object, so we don't manipulate the original 
+      const excludedFields = ['page', 'sort', 'limit', 'fields'];
+      excludedFields.forEach(el => delete queryObj[el]); //don't save a new array
+
+      console.log(req.query, queryObj); //shows the query in the log
+
+      const query = Tour.find(queryObj); //finds queried data in the collection (in the easiest way)
 
       // first way
-*/
-    
+
+    // const query = Tour.find()
+    // .where('duration')
+    // .equals(5)
+    // .where('difficulty')
+    // .equals('easy');
+
+// EXECUTE THE QUERY
+      const tours = query;
+
     res.status(200).json({
       status: "success",
       results: tours.length,
