be9c1284b01e8d9894043a605a7cc2b14109d6e3
GSbrend <brendon.gs.dev@gmail.com>
2025-08-16 11:23:01 -0300

    fix: correct formatting and validation logic in tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -16,12 +16,12 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlenght: [40 , 'A tour must have less or equal than 40 characters'], // str validators
       minlenght: [10 , 'A tour must have more or equal than 40 characters'], // str validators
-    validate: {
-        validator: function(value) {
-        return validator.isAlpha(value,split(' ').join(''));  
-      },
-      message: 'Tour name must only contain characters.'
-    }
+      validate: {
+          validator: function(value) {
+          return validator.isAlpha(value,split(' ').join(''));  
+        },
+        message: 'Tour name must only contain characters.'
+      }
     },
     slug: String,
     duration: {
@@ -35,17 +35,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
-          enum: { //enum is oly for strings
-      values: ['easy','medium','difficult'],
-      message: 'Difficulty is either easy, medium or difficult'
-    }
+      enum: { //enum is oly for strings
+        values: ['easy','medium','difficult'],
+        message: 'Difficulty is either easy, medium or difficult'
+      }
     },
     ratingsAverage: {
       type: Number,
       default: 0,
-          min: [1, 'Rating must be above 1.0'], // num / dates validators
-    max: [5, 'Rating must be below 5.0'] // num / dates validators
-
+      min: [1, 'Rating must be above 1.0'], // num / dates validators
+      max: [5, 'Rating must be below 5.0'] // num / dates validators
     },
     ratingsQuantity: {
       type: Number,
@@ -59,14 +58,13 @@ const tourSchema = new mongoose.Schema(
     summary: {
       type: String,
       trim: true, //cuts off all the white space at the beggining and ending of the input
-            validate: {
-        validator: function(val) {
-          // 'this' only gives access to create a NEW doc
-          return val < this.price;
-      },
-      message: 'Discount price ({VALUE}) should be below the regular price'
-    }
-
+        validate: {
+          validator: function(val) {
+            // 'this' only gives access to create a NEW doc
+            return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price'
+      }
     },
     desription: {
       type: String,
