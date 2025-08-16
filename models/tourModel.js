9aeffccf115f581469f696c0e570d63ea68b0915
GSbrend <brendon.gs.dev@gmail.com>
2025-08-16 10:38:00 -0300

    refactor: implement new validators on the schema, following the "fat models, thin controllers" philosophy; import validator library and create custom validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,6 +2,7 @@
 // the schema is used to define the structure of the documents within a collection
 const mongoose = require("mongoose");
 const slugify = require('slugify');
+const validator = require('validator');
 const tourSchema = new mongoose.Schema({
   // basic way to define a schema
   //name: String,
@@ -10,7 +11,15 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide a name"],
     unique: true,
-    trim: true
+    trim: true,
+    maxlenght: [40 , 'A tour must have less or equal than 40 characters'], // str validators
+    minlenght: [10 , 'A tour must have more or equal than 40 characters'], // str validators
+    validate: {
+        validator: function(value) {
+        return validator.isAlpha(value,split(' ').join(''));  
+      },
+      message: 'Tour name must only contain characters.'
+    }
   },
   slug: String,
   duration: {
@@ -23,11 +32,17 @@ const tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String,
-    required: [true, 'A tour must have a difficulty']
+    required: [true, 'A tour must have a difficulty'],
+    enum: { //enum is oly for strings
+      values: ['easy','medium','difficult'],
+      message: 'Difficulty is either easy, medium or difficult'
+    }
   },
   ratingsAverage: {
     type: Number,
     default: 0,
+    min: [1, 'Rating must be above 1.0'], // num / dates validators
+    max: [5, 'Rating must be below 5.0'] // num / dates validators
   },
   ratingsQuantity: {
     type: Number,
@@ -37,7 +52,16 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, "Please provide a price"],
   },
-  priceDiscount: Number,
+  priceDiscount: {
+    type: Number,
+      validate: {
+        validator: function(val) {
+          // 'this' only gives access to create a NEW doc
+          return val < this.price;
+      },
+      message: 'Discount price ({VALUE}) should be below the regular price'
+    }
+  },
   summary: {
     type: String,
     trim: true //cuts off all the white space at the beggining and ending of the input
