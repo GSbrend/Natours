bbeb5414297af1f5d8087e31da6d917d01769038
Brendon Garcia <Brendon.gs.dev@gmail.com>
2025-08-19 19:20:08 +0000

    refactor: improve error handling by adding uncaughtException and unhandledRejection listeners

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
@@ -9,13 +15,19 @@ const DB = process.env.DATABASE.replace(
   process.env.DB_PASSWORD
 );
 
-mongoose
-  .connect(DB)
-  .then(() => {
+mongoose.connect(DB).then(() => {
   console.log("DB connection successfull");
 });
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Servidor está rodando na porta ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
