ee2104190de39d9463754d3604dd603399bc6bf2
Brendon Garcia <Brendon.gs.dev@gmail.com>
2025-08-19 19:43:08 +0000

    implement: creating a user model for database CRUD operations

---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: {
+      validator: function (val) {
+        return validator.isAlpha(val.replace(/ /g, ""));
+      },
+      message: "Tour name must only contain characters",
+    },
+  },
+});
