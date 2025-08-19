cd031edee6650738ffbe00f4ba9b3b2345f8ea59
Brendon Garcia <Brendon.gs.dev@gmail.com>
2025-08-19 18:25:31 +0000

    refactor: quotes fix

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,8 +2,8 @@
 // the schema is used to define the structure of the documents within a collection
 const mongoose = require("mongoose");
 const slugify = require("slugify");
-const util = require('util');
-const validator = require('validator');
+const util = require("util");
+const validator = require("validator");
 const tourSchema = new mongoose.Schema(
   {
     // basic way to define a schema
@@ -14,14 +14,14 @@ const tourSchema = new mongoose.Schema(
       required: [true, "Please provide a name"],
       unique: true,
       trim: true,
-      maxlenght: [40 , 'A tour must have less or equal than 40 characters'], // str validators
-      minlenght: [10 , 'A tour must have more or equal than 10 characters'], // str validators
+      maxlenght: [40, "A tour must have less or equal than 40 characters"], // str validators
+      minlenght: [10, "A tour must have more or equal than 10 characters"], // str validators
       validate: {
-        validator: function(val) {
-         return validator.isAlpha(val.replace(/ /g, ''));
+        validator: function (val) {
+          return validator.isAlpha(val.replace(/ /g, ""));
         },
-        message: 'Tour name must only contain characters.'
-      }
+        message: "Tour name must only contain characters",
+      },
     },
     slug: String,
     duration: {
@@ -35,16 +35,17 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
-      enum: { //enum is oly for strings
-        values: ['easy','medium','difficult'],
-        message: 'Difficulty is either easy, medium or difficult'
-      }
+      enum: {
+        //enum is oly for strings
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either easy, medium or difficult",
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 0,
-      min: [1, 'Rating must be above 1.0'], // num / dates validators
-      max: [5, 'Rating must be below 5.0'] // num / dates validators
+      min: [1, "Rating must be above 1.0"], // num / dates validators
+      max: [5, "Rating must be below 5.0"], // num / dates validators
     },
     ratingsQuantity: {
       type: Number,
@@ -57,12 +58,12 @@ const tourSchema = new mongoose.Schema(
     priceDiscount: {
       type: Number,
       validate: {
-        validator: function(val) {
+        validator: function (val) {
           // 'this' only gives access to create a NEW doc
           return val < this.price;
         },
-        message: 'Discount price ({VALUE}) should be below the regular price'
-      }
+        message: "Discount price ({VALUE}) should be below the regular price",
+      },
     },
     summary: {
       type: String,
@@ -127,8 +128,8 @@ tourSchema.post(/^find/, function (docs, next) {
 
 // AGGREGATION MIDDLEWARE
 // middleware that runs before or after any aggregation happens on code
-tourSchema.pre('aggregate', function (next) {
-  this._pipeline.unshift({$match:{secretTour:{$ne: true}}});
+tourSchema.pre("aggregate", function (next) {
+  this._pipeline.unshift({ $match: { secretTour: { $ne: true } } });
   // console.log(util.inspect(this._pipeline,false,null,true));
   next();
 });
